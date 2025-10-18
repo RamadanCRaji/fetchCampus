958dce41530f5b608f3fa7b35b3eb50bf124f6f2
RamadanCRaji <armadanraji@gmail.com>
2025-10-18 18:47:02 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Ramadan Raji on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
